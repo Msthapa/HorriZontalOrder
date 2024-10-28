4359db374e84d673332fd0c01b72c6f3a56db56d
Msthapa <1995msthapa@gmail.com>
2024-10-28 23:20:42 +0530

    Defined Activity class and viewModel independently with initial hilt

---
+++ app/src/main/java/com/example/horrizontalorder/view/MyOrdersActivity.kt
@@ -0,0 +1,8 @@
+package com.example.horrizontalorder.view
+
+import android.app.Activity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MyOrdersActivity : Activity() {
+}
---
+++ app/src/main/java/com/example/horrizontalorder/viewModel/MyOrdersViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.horrizontalorder.viewModel
+
+class MyOrdersViewModel {
+}
