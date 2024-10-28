ebf60d157509ead57ab865314dc79c6f2ca9cd26
Msthapa <1995msthapa@gmail.com>
2024-10-28 23:22:40 +0530

    changed activity type to back-ward compatible

--- app/src/main/java/com/example/horrizontalorder/view/MyOrdersActivity.kt
+++ app/src/main/java/com/example/horrizontalorder/view/MyOrdersActivity.kt
@@ -1,8 +1,9 @@
 package com.example.horrizontalorder.view
 
 import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MyOrdersActivity : Activity() {
+class MyOrdersActivity : AppCompatActivity() { // for back-ward compatibility support
 }
